1dd9d0e40b8892b2f41fb88fc20637b687a1e66e
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2024-07-18 15:09:04 +0700

    test: add tests

--- tests/username_from_transformer_checker.rs
+++ tests/username_from_transformer_checker.rs
@@ -2,17 +2,19 @@ use subtype::constraints::not::Not;
 use subtype::constraints::Empty;
 use subtype::constructor_from_transformer_checker;
 use subtype::errors::InvalidValueError;
+use subtype::transformers::trim::Trimmed;
 
 #[allow(dead_code)]
 #[derive(PartialOrd, PartialEq, Clone, Debug)]
 pub struct Username(String);
 
 impl Username {
-    constructor_from_transformer_checker!(pub fn new, String | Not<Empty>, tuple, value);
+    constructor_from_transformer_checker!(pub fn new, String [Trimmed] | Not<Empty>, tuple, value);
 }
 
 #[test]
 fn username_from_transformer_checker() {
     assert_eq!(Username::new(""), Err(InvalidValueError::<String, Not<Empty>>::new("")));
+    assert_eq!(Username::new("   "), Err(InvalidValueError::<String, Not<Empty>>::new("")));
     assert_eq!(Username::new("alice"), Ok(Username("alice".to_string())));
 }
