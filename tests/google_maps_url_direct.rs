dca69da6f67c3f189c03c091f36affa79b4ae85b
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2025-12-27 15:49:54 +0700

    fix: google_maps_url_direct

--- tests/google_maps_url_direct.rs
+++ tests/google_maps_url_direct.rs
@@ -1,35 +1,77 @@
 use derive_more::{AsRef, Deref, Into};
 use subtype::impl_try_from_ref_via_owned;
+use thiserror::Error;
 use url::Url;
 
 #[derive(Deref, AsRef, Into, Ord, PartialOrd, Eq, PartialEq, Hash, Clone, Debug)]
 pub struct GoogleMapsUrl(Url);
 
-impl TryFrom<Url> for GoogleMapsUrl {
-    type Error = ();
+#[derive(Error, Debug, Eq, PartialEq)]
+pub enum GoogleMapsUrlFromUrlError {
+    #[error("url is missing a host")]
+    MissingHost,
+    #[error("url host is not a Google Maps host: {host}")]
+    InvalidHost { host: String },
+    #[error("google.com urls must start with /maps, got: {path}")]
+    InvalidPath { path: String },
+}
 
-    fn try_from(_url: Url) -> Result<Self, Self::Error> {
-        // TODO: validate the url
-        // TODO: pass if domain == maps.app.goo.gl
-        // TODO: pass if domain ends with google.com and path starts with maps
-        todo!()
+impl TryFrom<Url> for GoogleMapsUrl {
+    type Error = GoogleMapsUrlFromUrlError;
+
+    fn try_from(url: Url) -> Result<Self, Self::Error> {
+        use GoogleMapsUrlFromUrlError::*;
+        let host = url.host_str().ok_or(MissingHost)?;
+        let is_shortlink = host == "maps.app.goo.gl";
+        let is_google_host = host == "google.com" || host.ends_with(".google.com");
+        let path_starts_with_maps = url
+            .path_segments()
+            .and_then(|mut segments| segments.next().map(|segment| segment == "maps"))
+            .unwrap_or(false);
+        if is_shortlink {
+            return Ok(Self(url));
+        }
+        if !is_google_host {
+            return Err(InvalidHost {
+                host: host.to_string(),
+            });
+        }
+        if !path_starts_with_maps {
+            return Err(InvalidPath {
+                path: url.path().to_string(),
+            });
+        }
+        Ok(Self(url))
     }
 }
 
 impl_try_from_ref_via_owned!(impl TryFrom<&Url> for GoogleMapsUrl, Url);
 
 impl TryFrom<String> for GoogleMapsUrl {
-    type Error = ();
+    type Error = GoogleMapsUrlFromStringError;
 
-    fn try_from(_value: String) -> Result<Self, Self::Error> {
-        // TODO: parse url, then call Self::try_from
-        todo!()
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let url = Url::parse(&value)?;
+        Ok(GoogleMapsUrl::try_from(url)?)
     }
 }
 
 impl_try_from_ref_via_owned!(impl TryFrom<&str> for GoogleMapsUrl, String);
 
-#[ignore]
+#[derive(Error, Debug)]
+pub enum GoogleMapsUrlFromStringError {
+    #[error("failed to parse url")]
+    Parse {
+        #[from]
+        source: url::ParseError,
+    },
+    #[error("invalid google maps url")]
+    InvalidUrl {
+        #[from]
+        source: GoogleMapsUrlFromUrlError,
+    },
+}
+
 #[test]
 fn must_try_from_url() {
     assert!(GoogleMapsUrl::try_from("https://maps.app.goo.gl/v42d7shTot2QmUcF9").is_ok());
